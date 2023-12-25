e601cdd2bd3be87ef25b25bf795ce9be150d830b
cfrehr <cfrehr@gmail.com>
2023-12-25 13:23:08 -0800

    fixes kotlin test configs (#22)

---
+++ common/src/test/kotlin/io/xpdftools/common/util/ReadInputStreamTaskTest.kt
@@ -0,0 +1,24 @@
+package io.xpdftools.common.util
+
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import java.io.InputStream
+
+class ReadInputStreamTaskTest {
+
+    @Test
+    fun `should call and return lines`() {
+        // given
+        val inputStream = mockk<InputStream>()
+        // when
+
+        // then
+
+    }
+
+    @Test
+    fun `should call and return null`() {
+
+    }
+
+}
