d1f14af2d6a05db298aa9695c7c7c1810bb16041
Cody Frehr <cfrehr@gmail.com>
2024-01-15 18:58:10 -0800

    fixes autoconfigure

--- pdf-text-spring-boot-autoconfigure/src/main/java/io/xpdftools/pdftext/autoconfigure/PdfTextToolProperties.java
+++ pdf-text-spring-boot-autoconfigure/src/main/java/io/xpdftools/pdftext/autoconfigure/PdfTextToolProperties.java
@@ -7,11 +7,11 @@ import java.nio.file.Path;
 
 //todo: javadoc
 @Data
-@ConfigurationProperties(prefix = "xpdftools.pdf-text")
+@ConfigurationProperties(prefix = "xpdf-tools.pdf-text")
 public class PdfTextToolProperties {
 
     //todo: what is correct way to deal with "Path" type?
-    private Path libraryPath;
+    private Path nativeLibraryPath;
     private String defaultOutputDirectory;
     //todo: can we use "Long" or should it be primitive?
     private Long timeoutMilliseconds;
--- pdf-text/src/main/java/io/xpdftools/pdftext/config/PdfTextToolConfigParams.java
+++ pdf-text/src/main/java/io/xpdftools/pdftext/config/PdfTextToolConfigParams.java
@@ -2,7 +2,7 @@ package io.xpdftools.pdftext.config;
 
 //todo: javadoc
 public class PdfTextToolConfigParams {
-    public static final String LIBRARY_PATH = "library-path";
+    public static final String NATIVE_LIBRARY_PATH = "native-library-path";
     public static final String DEFAULT_OUTPUT_DIRECTORY = "default-output-directory";
     public static final String TIMEOUT_MILLISECONDS = "timeout-milliseconds";
 
