60ea44b2c966ae56c64b3f84979b9b8c0d9b8c1f
cfrehr <cfrehr@gmail.com>
2024-01-20 14:28:26 -0800

    cleans up autoconfiguration (#33)

--- pdf-text-spring-boot-starter/src/main/java/io/xpdftools/pdftext/autoconfigure/PdfTextToolAutoConfiguration.java
+++ pdf-text-spring-boot-starter/src/main/java/io/xpdftools/pdftext/autoconfigure/PdfTextToolAutoConfiguration.java
@@ -2,8 +2,6 @@ package io.xpdftools.pdftext.autoconfigure;
 
 import io.xpdftools.common.util.XpdfUtils;
 import io.xpdftools.pdftext.PdfTextTool;
-import io.xpdftools.pdftext.config.PdfTextToolConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -11,8 +9,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 import java.nio.file.Path;
-
-import static io.xpdftools.pdftext.config.PdfTextToolConfigParams.*;
 
 /**
  * An autoconfiguration for a {@code PdfTextTool} bean, enabling configuration properties in {@code PdfTextToolProperties}.
@@ -25,12 +21,9 @@ import static io.xpdftools.pdftext.config.PdfTextToolConfigParams.*;
 @EnableConfigurationProperties(PdfTextToolProperties.class)
 public class PdfTextToolAutoConfiguration {
 
-    @Autowired
-    private PdfTextToolProperties pdfTextToolProperties;
-
     @Bean
     @ConditionalOnMissingBean
-    public PdfTextToolConfig pdfTextToolConfig() {
+    public PdfTextTool pdfTextTool(PdfTextToolProperties pdfTextToolProperties) {
         Path nativeLibraryPath = pdfTextToolProperties.getNativeLibraryPath() == null
                 ? XpdfUtils.getPdfTextNativeLibraryPath()
                 : pdfTextToolProperties.getNativeLibraryPath();
@@ -43,21 +36,10 @@ public class PdfTextToolAutoConfiguration {
                 ? XpdfUtils.getPdfTextTimeoutMilliseconds()
                 : pdfTextToolProperties.getTimeoutMilliseconds();
 
-        PdfTextToolConfig pdfTextToolConfig = new PdfTextToolConfig();
-        pdfTextToolConfig.put(NATIVE_LIBRARY_PATH, nativeLibraryPath);
-        pdfTextToolConfig.put(DEFAULT_OUTPUT_PATH, defaultOutputPath);
-        pdfTextToolConfig.put(TIMEOUT_MILLISECONDS, timeoutMilliseconds);
-
-        return pdfTextToolConfig;
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public PdfTextTool pdfTextTool(PdfTextToolConfig pdfTextToolConfig) {
         return PdfTextTool.builder()
-                .nativeLibraryPath((Path) pdfTextToolConfig.get(NATIVE_LIBRARY_PATH))
-                .defaultOutputPath((Path) pdfTextToolConfig.get(DEFAULT_OUTPUT_PATH))
-                .timeoutMilliseconds((Long) pdfTextToolConfig.get(TIMEOUT_MILLISECONDS))
+                .nativeLibraryPath(nativeLibraryPath)
+                .defaultOutputPath(defaultOutputPath)
+                .timeoutMilliseconds(timeoutMilliseconds)
                 .build();
     }
 
--- pdf-text/src/main/java/io/xpdftools/pdftext/config/PdfTextToolConfig.java
+++
@@ -1,9 +0,0 @@
-package io.xpdftools.pdftext.config;
-
-import java.util.Properties;
-
-//todo: javadoc
-public class PdfTextToolConfig extends Properties {
-    private static final long serialVersionUID = 5662570853707247891L;
-
-}
