6ce4df98492314873b52347ca04178db3561501f
Cody Frehr <cfrehr@gmail.com>
2024-01-30 07:58:35 -0800

    fixes tests

--- common/src/test/kotlin/io/xpdftools/common/util/ReadInputStreamTaskTest.kt
+++ common/src/test/kotlin/io/xpdftools/common/util/ReadInputStreamTaskTest.kt
@@ -1,30 +1,30 @@
 package io.xpdftools.common.util
 
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockkStatic
-import io.mockk.unmockkStatic
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayInputStream
 
 class ReadInputStreamTaskTest {
 
-    //todo: fix this test and remove @Disabled!
-    @Disabled
+    private val originalLineSeparator = System.getProperty("line.separator")
+
+    @AfterEach
+    fun afterEach() {
+        // reset system properties that test may have altered
+        System.setProperty("line.separator", originalLineSeparator)
+    }
+
     @Test
     fun `should call and return lines`() {
         // given
-        mockkStatic(System::class)
-        every { System.lineSeparator() } returns "\n"
+        System.setProperty("line.separator", " END ")
 
         val inputStream = ByteArrayInputStream("line1\r\nline2\r\nline3".toByteArray())
         val readInputStreamTask = ReadInputStreamTask(inputStream)
 
         // when then
-        readInputStreamTask.call() shouldBe "line1\nline2\nline3"
-
-        unmockkStatic(System::class)
+        readInputStreamTask.call() shouldBe "line1 END line2 END line3"
     }
 
     @Test
