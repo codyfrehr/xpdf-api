60c18342a66d1d3341ea755fa15b7ec2b7d16a72
Cody Frehr <cfrehr@gmail.com>
2024-02-12 16:47:44 -0800

    Revert "restructures project (#70)" (#71)

---
+++ common-api/src/main/java/io/xpdf/api/common/exception/XpdfProcessingException.java
@@ -0,0 +1,29 @@
+/*
+ * Common - A library of common components.
+ * Copyright © 2024 xpdf.io (info@xpdf.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.xpdf.api.common.exception;
+
+import lombok.experimental.StandardException;
+
+/**
+ * A {@link XpdfException} thrown during the processing of a <em>Xpdf</em> request.
+ *
+ * @since 1.0.0
+ */
+@StandardException
+public class XpdfProcessingException extends XpdfException {
+
+}
---
+++ pdf-text-api-spring-boot-starter/src/main/java/io/xpdf/api/pdftext/autoconfigure/PdfTextToolProperties.java
@@ -0,0 +1,37 @@
+/*
+ * PdfText API Starter - A Spring Boot starter for PdfText API.
+ * Copyright © 2024 xpdf.io (info@xpdf.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.xpdf.api.pdftext.autoconfigure;
+
+import io.xpdf.api.pdftext.PdfTextTool;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.nio.file.Path;
+
+/**
+ * The configurable properties of a {@link PdfTextTool} that is autoconfigured in {@link PdfTextToolAutoConfiguration}.
+ *
+ * @since 1.0.0
+ */
+@Data
+@ConfigurationProperties(prefix = "xpdf-api.pdf-text")
+public class PdfTextToolProperties {
+
+    private Path nativeLibraryPath;
+    private Integer timeoutSeconds;
+
+}
