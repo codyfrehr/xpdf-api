d9929869b7c51dfbbf5fa552718442eb05adf1a9
Cody Frehr <cfrehr@gmail.com>
2024-02-12 16:09:05 -0800

    restructures project (#70)

---
+++ xpdf-api/src/main/java/io/xpdf/api/common/exception/XpdfException.java
@@ -0,0 +1,29 @@
+/*
+ * Xpdf API - A collection of APIs for interacting with native Xpdf libraries.
+ * Copyright © 2024 xpdf.io (info@xpdf.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.xpdf.api.common.exception;
+
+import lombok.experimental.StandardException;
+
+/**
+ * A <em>Xpdf</em> {@code Exception}.
+ *
+ * @since 1.0.0
+ */
+@StandardException
+public class XpdfException extends Exception {
+
+}
