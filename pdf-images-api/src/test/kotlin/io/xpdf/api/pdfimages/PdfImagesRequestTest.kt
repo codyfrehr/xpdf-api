b41dd967dee29967de4d07e0b49c3e56f2504b5e
Cody Frehr <cfrehr@gmail.com>
2025-03-22 20:49:03 -0400

    adds licensing

--- pdf-images-api/src/test/kotlin/io/xpdf/api/pdfimages/PdfImagesRequestTest.kt
+++ pdf-images-api/src/test/kotlin/io/xpdf/api/pdfimages/PdfImagesRequestTest.kt
@@ -1,3 +1,19 @@
+/*
+ * PdfImages API - An API for accessing a native pdfimages library (https://xpdf.io)
+ * Copyright © 2025 xpdf.io (info@xpdf.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.xpdf.api.pdfimages
 
 import io.kotest.assertions.throwables.shouldThrow
